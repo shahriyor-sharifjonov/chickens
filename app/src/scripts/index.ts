258b26a2269fdb22ece198f3cd250fdbc39a4495
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-18 14:53:16 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -1,5 +1,23 @@
 $('.about__slider').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 467,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+  ]
 });
