212b022b19484a8419dad5a271d371e592abf58b
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-19 13:23:45 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -21,3 +21,13 @@ $('.about__slider').slick({
     },
   ]
 });
+
+function openDrop(){
+  const popup = document.querySelector('.popup');
+  popup.classList.add('active')
+}
+
+function closePopup(){
+  const popup = document.querySelector('.popup');
+  popup.classList.remove('active')
+}
