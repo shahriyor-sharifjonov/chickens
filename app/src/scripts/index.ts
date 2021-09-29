48cf048b25e96b80c7445a3c18a9630e3511abd1
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-29 20:00:34 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -31,3 +31,23 @@ function closePopup(){
   const popup = document.querySelector('.popup');
   popup.classList.remove('active')
 }
+
+function openVideo(){
+  const video = document.querySelector('.video');
+  video.classList.add('active')
+}
+
+function closeVideo(){
+  const video = document.querySelector('.video');
+  video.classList.remove('active')
+}
+
+window.addEventListener('click', function(e){
+  console.log(e.target);
+  
+  if(e.target.classList.contains('video__close')){
+    console.log('asasgf');
+    const video = document.querySelector('.video');
+    video.classList.remove('active')
+  }
+})
